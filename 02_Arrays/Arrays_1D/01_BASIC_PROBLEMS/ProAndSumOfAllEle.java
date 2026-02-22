54b0cd6a09163a4d1d06ff960ef7dfcd02dad6d9
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-22 08:10:05 +0530

    UIpdate

---
+++ 02_Arrays/Arrays_1D/01_BASIC_PROBLEMS/ProAndSumOfAllEle.java
@@ -0,0 +1,23 @@
+public class ProAndSumOfAllEle {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 5, 3, 45, 52 };
+        System.out.println(product(arr));
+        System.out.println(sum(arr));
+    }
+
+    public static int product(int[] arr) {
+        int product = 1;
+        for (int val : arr) {
+            product *= val;
+        }
+        return product;
+    }
+
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int val : arr) {
+            sum += val;
+        }
+        return sum;
+    }
+}
